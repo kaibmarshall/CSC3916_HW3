c7e4208dfb6489362aa5989e5dadd926c82b9af0
kaibmarshall <kaibmarshall@gmail.com>
2022-04-23 14:16:09 -0600

    Changes for HW5

--- server.js
+++ server.js
@@ -260,15 +260,14 @@ router.route('/reviews')
                                 }
                         },
                         {$group: {_id: '', "review_sum": {$sum: "$rating"}}},
-                        {$project: {_id: 0, "review_sum": '$review_sum'}}
-
-                // {
-                        //     $match:
-                        //         {
-                        //             movieID: movie._id
-                        //         }
-                        // },
-                        // {$count : "num_of_reviews"}
+                        {$project: {_id: 0, "review_sum": '$review_sum'}},
+                        {
+                            $match:
+                                {
+                                    movieID: movie._id
+                                }
+                        },
+                        {$count : "num_of_reviews"}
                     ]
                 Review.aggregate(pipeline, function(err, result) {
                     res.json(result);
