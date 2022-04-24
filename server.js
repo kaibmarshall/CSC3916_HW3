c9bbf67c76e885c85781985fa4daa62daa06ec4a
kaibmarshall <kaibmarshall@gmail.com>
2022-04-24 10:04:27 -0600

    Changes for HW5

--- server.js
+++ server.js
@@ -267,12 +267,12 @@ router.route('/reviews')
                         {$group: {_id: '', "review_avg": {$avg: "$rating"}}},
                     ]
                 Review.aggregate(pipeline, function(err, result) {
-                    result = JSON.parse(result)
+                    result_JSON = JSON.parse(result)
                     Movie.updateOne(
                         {title:req.body.movieTitle},
-                        {$set: {"avgRating" : result.review_avg}}
+                        {$set: {"avgRating" : result_JSON.review_avg}}
                     )
-                    res.json({success: true, msg: 'Successfully created new review.', new_movie_avg_rating: result.review_avg});
+                    res.json({success: true, msg: 'Successfully created new review.', new_movie_avg_rating: result_JSON.review_avg});
                 });
 
 
