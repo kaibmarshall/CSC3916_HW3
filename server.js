6029ba80bac5e2c494823ace9a33dc5e19f12538
kaibmarshall <kaibmarshall@gmail.com>
2022-04-24 10:20:06 -0600

    Changes for HW5

--- server.js
+++ server.js
@@ -267,11 +267,12 @@ router.route('/reviews')
                         {$group: {_id: '', "review_avg": {$avg: "$rating"}}},
                     ]
                 Review.aggregate(pipeline, function(err, result) {
-                    Movie.updateOne(
-                        {title:req.body.movieTitle},
-                        {$set: {"avgRating" : result.body.review_avg}}
-                    )
-                    res.json({success: true, msg: 'Successfully created new review.', new_movie_avg_rating: result.body.review_avg});
+                    res.json(result);
+                    // Movie.updateOne(
+                    //     {title:req.body.movieTitle},
+                    //     {$set: {"avgRating" : result.review_avg}}
+                    // )
+                    // res.json({success: true, msg: 'Successfully created new review.', new_movie_avg_rating: result.review_avg});
                 });
 
 
